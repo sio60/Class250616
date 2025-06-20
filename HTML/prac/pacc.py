9b1b045040932dc4f16eb0ddbf322531f8a11dc7
yuna <yuna5470@naver.com>
2025-06-20 14:14:36 +0900

    javascript

---
+++ HTML/prac/pacc.py
@@ -0,0 +1,11 @@
+def menu():
+
+print(menu)
+user=int(input())
+
+while True:
+    num1=float(input("num.1: "))
+    num2=float(input("num.2: "))
+    if user == 1:
+        print(num1+num2)
+        
